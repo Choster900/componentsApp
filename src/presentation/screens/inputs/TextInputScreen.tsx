49f6677a72c257ce24cfada3185357814baa8dc1
Sergio.Lopez <salopez@innovacion.gob.sv>
2025-07-16 09:55:21 -0600

    feat: Create TextInputScreen component for user input functionality

---
+++ src/presentation/screens/inputs/TextInputScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import CustomView from '../../components/ui/CustomView'
+import { TextInput } from 'react-native'
+import { Title } from '../../components/ui/Title'
+import Card from '../../components/ui/Card';
+
+export default function TextInputScreen() {
+    return (
+        <CustomView style={{}} margin>
+
+
+            <Card>
+                <TextInput />
+            </Card>
+
+        </CustomView>
+    )
+}
